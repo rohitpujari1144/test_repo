f40cbb4f321a1fd59892a4fc08a1ddcdcbd0646a
Rohit Karan Pujari <113190799+rohitpujari1144@users.noreply.github.com>
2023-08-14 23:12:18 +0530

    Add files via upload

---
+++ Backend/config/dbConfig.js
@@ -0,0 +1,6 @@
+const mongodb = require('mongodb')
+const MongoClient=mongodb.MongoClient
+const dbName='TestDB'
+const dbUrl=`mongodb+srv://rohit10231:rohitkaranpujari@cluster0.kjynvxt.mongodb.net/${dbName}`
+
+module.exports={mongodb, MongoClient, dbName, dbUrl}

9ea3db7bc9193844597db7d6866b2cfaf9ba7e87
Rohit Karan Pujari <113190799+rohitpujari1144@users.noreply.github.com>
2023-08-19 22:07:45 +0530

    Delete Backend directory

--- Backend/config/dbConfig.js
+++
@@ -1,8 +0,0 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
-const dbName = 'TestDB'
-const dbUrl = `mongodb+srv://rohit10231:rohitkaranpujari@cluster0.kjynvxt.mongodb.net/${dbName}`
-const client = new MongoClient(dbUrl)
-const db = client.db(dbName)
-
-module.exports = { MongoClient, dbName, dbUrl }
